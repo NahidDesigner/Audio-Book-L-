02b3b7708c2628941f17e9b6009f83f9c1e7eb3d
Najmul Hossain <nahid@Najmuls-Mac-mini.local>
2026-02-21 10:17:20 +0600

    Update project

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", "**/._*"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
